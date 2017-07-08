75c516ecddda48892a947e66a72b933f7127991e
julian-arroyave <julian.arroyave@newshore.es>
2017-07-07 19:57:47 -0500

    [JJSERVICIOS] Versioning Web

---
+++ JJServicios.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JJServicios.Web.MvcApplication" Language="C#" %>
---
+++ JJServicios.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JJServicios.Web.Startup))]
+namespace JJServicios.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
